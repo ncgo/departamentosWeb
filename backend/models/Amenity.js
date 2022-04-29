ef9809dd12c6581de65c08390ef3087d0800d3fb
adriangzz <adriangonzalezmontemayor@gmail.com>
2022-04-28 20:07:31 -0500

    Typo in name

---
+++ backend/models/Amenity.js
@@ -0,0 +1,53 @@
+// # 1
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var bcrypt = require("bcrypt");
+
+// # 2
+var AmenitiesSchema = Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    services: [
+      {
+        id: {
+          type: Number,
+          required: true,
+        },
+        userName: {
+          type: String,
+          required: true,
+        },
+        fecha_reservada: [
+          {
+            id: {
+              type: Schema.ObjectId,
+              required: true,
+            },
+            date: {
+              type: Date,
+              required: true,
+            },
+            required: false,
+          },
+        ],
+        required: false,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// # 3
+module.exports = mongoose.model("amenities", AmenitiesSchema);
