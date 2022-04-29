f2281ed46a4467af495e6b109dc04e815daacfac
adriangzz <adriangonzalezmontemayor@gmail.com>
2022-04-28 20:12:22 -0500

    Add tower model

---
+++ backend/models/Tower.js
@@ -0,0 +1,53 @@
+// # 1
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// # 2
+var reportSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      street_1: {
+        type: String,
+        required: true,
+      },
+      street_2: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      postal_code: {
+        type: Number,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+    },
+    apartments: [
+      {
+        type: Schema.ObjectId,
+        ref: "apartments",
+        required: false,
+      },
+    ],
+    amenities: [
+      {
+        type: Schema.ObjectId,
+        ref: "amenities",
+        required: false,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// # 3
+module.exports = mongoose.model("reports", reportSchema);
