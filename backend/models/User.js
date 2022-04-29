2304ac6cf06d47f89f7bfad3b06cc9802d1ce22f
adriangzz <adriangonzalezmontemayor@gmail.com>
2022-04-28 19:31:48 -0500

    Update user fields

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,56 +1,69 @@
 // # 1
-var mongoose = require("mongoose")
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var bcrypt = require("bcrypt");
 
 // # 2
-var UserSchema = Schema ({
-    
+var UserSchema = Schema(
+  {
     firstName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     birthDate: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     role: {
-        type: String,
-        default: "tenant",
-        required: true
+      type: String,
+      default: "tenant",
+      required: true,
     },
-    phone:{
-        type: String,
-        required: true
+    phone: {
+      type: String,
+      required: true,
     },
-    apartment: {
-        type: String,
-        required: true,
-        default: "N/A"
+    tower: {
+      type: Schema.ObjectId,
+      ref: "torres",
+      required: false,
     },
-},{timestamps: true});
+    aparment: {
+      type: Schema.ObjectId,
+      ref: "apartments",
+      required: false,
+    },
+    administers_tower: [
+      {
+        type: Schema.ObjectId,
+        ref: "towers",
+        required: false,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-UserSchema.methods.encryptPassword = function(password){
-    return bcrypt.hashSync(password,10)
-}
+UserSchema.methods.encryptPassword = function (password) {
+  return bcrypt.hashSync(password, 10);
+};
 
-UserSchema.methods.validatePassword = function(userPassword){
-    return bcrypt.compare(userPassword,this.password)
-}
-
+UserSchema.methods.validatePassword = function (userPassword) {
+  return bcrypt.compare(userPassword, this.password);
+};
 
 // # 3
-module.exports = mongoose.model('users', UserSchema);
+module.exports = mongoose.model("users", UserSchema);
