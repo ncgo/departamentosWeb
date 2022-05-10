a0c62db070007ec9dca028f3d3c44fc4a4b25826
adriangzz <adriangonzalezmontemayor@gmail.com>
2022-05-10 15:05:52 -0500

    Style header

--- uchi/src/components/Layout/Header.vue
+++ uchi/src/components/Layout/Header.vue
@@ -4,9 +4,30 @@ import { RouterLink } from "vue-router";
 
 <template>
   <header>
-    <div class="wrapper">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/reserve">Reserve</RouterLink>
-    </div>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/reserve">Reserve</RouterLink>
   </header>
 </template>
+
+<style scoped>
+header {
+  display: flex;
+  -webkit-box-shadow: 0px 5px 15px -9px rgba(0, 0, 0, 0.27);
+  box-shadow: 0px 5px 15px -9px rgba(0, 0, 0, 0.27);
+  padding: 0 1.5rem;
+  height: 3.6rem;
+  background-color: #e4355c;
+  align-items: center;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+  margin-right: 0.5rem;
+  font-size: 1.1rem;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
