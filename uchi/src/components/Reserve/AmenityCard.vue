acf83628495ec34fc5e23d4f7e580a018d5f8de1
adriangzz <adriangonzalezmontemayor@gmail.com>
2022-05-10 18:55:06 -0500

    Add amenities to reserve

---
+++ uchi/src/components/Reserve/AmenityCard.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+defineProps<{
+  amenity: object;
+}>();
+</script>
+
+<template>
+  <RouterLink :to="'/reserve/' + amenity._id">
+    <li class="wrapper">
+      <img
+        src="https://hispanosemprendedores.com/wp-content/uploads/2021/02/laundry-machines-J9AM5SS.jpg"
+      />
+      <h3>{{ amenity.name }}</h3>
+      <p>{{ amenity.description }}</p>
+    </li>
+  </RouterLink>
+</template>
+
+<style scoped>
+.wrapper {
+  height: 19rem;
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-end;
+  border-radius: 0.3rem;
+  overflow: hidden;
+}
+
+.wrapper:after {
+  content: "";
+  position: absolute;
+  inset: 0px;
+  pointer-events: none;
+  border-radius: 8px;
+  transform: translateZ(0px);
+  transition: opacity 300ms ease 0s;
+  background: rgba(0, 0, 0, 0)
+    linear-gradient(
+      transparent 62%,
+      rgba(0, 0, 0, 0.004) 63.94%,
+      rgba(0, 0, 0, 0.016) 65.89%,
+      rgba(0, 0, 0, 0.03) 67.83%,
+      rgba(0, 0, 0, 0.06) 69.78%,
+      rgba(0, 0, 0, 0.094) 71.72%,
+      rgba(0, 0, 0, 0.133) 73.67%,
+      rgba(0, 0, 0, 0.176) 75.61%,
+      rgba(0, 0, 0, 0.224) 77.56%,
+      rgba(0, 0, 0, 0.267) 79.5%,
+      rgba(0, 0, 0, 0.306) 81.44%,
+      rgba(0, 0, 0, 0.34) 83.39%,
+      rgba(0, 0, 0, 0.37) 85.33%,
+      rgba(0, 0, 0, 0.384) 87.28%,
+      rgba(0, 0, 0, 0.396) 89.22%,
+      rgba(0, 0, 0, 0.4) 91.17%
+    )
+    repeat scroll 0% 0%;
+  opacity: 1;
+}
+
+img {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+h3 {
+  color: white;
+  left: 1rem;
+  bottom: 0.7rem;
+  font-size: 1.3rem;
+  z-index: 9;
+}
+
+p {
+  color: white;
+  left: 1rem;
+  bottom: 1rem;
+  z-index: 9;
+}
+</style>
--- uchi/src/views/ReserveView.vue
+++ uchi/src/views/ReserveView.vue
@@ -1,13 +1,52 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { onMounted } from "vue";
+
+import AmenityCard from "../components/Reserve/AmenityCard.vue";
+
+const amenitiesRef = ref([]);
+const disabled = ref(true);
+
+const getAmenities = async () => {
+  const amenities = await fetch(`${import.meta.env.VITE_HOST}/api/amenity`);
+  const amenitiesJson = await amenities.json();
+  if (amenitiesJson.amenities) {
+    amenitiesRef.value = amenitiesJson.amenities;
+  }
+};
+
+onMounted(() => {
+  getAmenities();
+});
+</script>
 
 <template>
   <main>
-    <h1>Reserve</h1>
+    <h1>Amenities</h1>
+    <ul class="grid">
+      <AmenityCard
+        v-for="amenity in amenitiesRef"
+        :key="amenity._id"
+        :amenity="amenity"
+      />
+    </ul>
   </main>
 </template>
 
 <style scoped>
 main {
   padding: 1.5rem;
+}
+
+ul {
+  padding: 0;
+}
+
+.grid {
+  display: grid;
+  list-style: none;
+  grid-template-columns: repeat(auto-fill, minmax(360px, 1fr));
+  gap: 30px;
+  margin-top: 1rem;
 }
 </style>
