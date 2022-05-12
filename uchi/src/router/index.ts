108dc9032e787bede05401392eb694ff87d1e298
nadia.co <ngarciaorozco@outlook.com>
2022-05-12 13:09:13 -0500

    routes report

--- uchi/src/router/index.ts
+++ uchi/src/router/index.ts
@@ -36,7 +36,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/ReportView.vue"),
+      component: () => import("../views/Reports/ReportsView.vue"),
     },
     {
       path: "/messages",
