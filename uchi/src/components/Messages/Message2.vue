0eb8555b5ece0e968aa25eaf770bbf10ca8aa2e7
Ulrik Nuño <62920394+Ulrikcpp@users.noreply.github.com>
2022-05-12 16:43:02 -0500

    Add files via upload

---
+++ uchi/src/components/Messages/Message2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="message">
+    <span v-if="!sender">{{ name }}</span>
+    <div class="flex" :class="sender ? 'flex-row-reverse' : ''">
+      <Avatar class="mt-1" :src="photoUrl" />
+      <div class="text w-3/4" :class="sender ? 'bg-purple-800' : 'bg-purple-700'">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Avatar from './Avatar.vue'
+export default {
+  components: { Avatar },
+  props: {
+    name: { type: String, default: '' },
+    photoUrl: { type: String, default: '' },
+    sender: { type: Boolean, default: false }
+  }
+}
+</script>
