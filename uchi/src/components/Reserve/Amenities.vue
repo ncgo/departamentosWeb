287559a38c723e50c1315a33cccfaac9173416c0
adriangzz <adriangonzalezmontemayor@gmail.com>
2022-05-16 17:11:47 -0500

    Move amenities to seperate component

---
+++ uchi/src/components/Reserve/Amenities.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { onMounted } from "vue";
+
+import AmenityCard from "./AmenityCard.vue";
+
+const amenitiesRef = ref([]);
+const disabled = ref(true);
+
+const getAmenities = async () => {
+  const amenities = await fetch(`${import.meta.env.VITE_HOST}/api/amenity`);
+  const amenitiesJson = await amenities.json();
+  if (amenitiesJson.amenities) {
+    amenitiesRef.value = amenitiesJson.amenities;
+  }
+};
+
+onMounted(() => {
+  getAmenities();
+});
+</script>
+
+<template>
+  <ul class="grid">
+    <AmenityCard
+      v-for="amenity in amenitiesRef"
+      :key="amenity['_id']"
+      :amenity="amenity"
+    />
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  padding: 0;
+}
+
+.grid {
+  display: grid;
+  list-style: none;
+  grid-template-columns: repeat(auto-fill, minmax(360px, 1fr));
+  gap: 30px;
+  margin-top: 1rem;
+}
+</style>
