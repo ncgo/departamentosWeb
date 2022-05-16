92e127780b04761fc39e6e14bda9c0f99c3e0454
adriangzz <adriangonzalezmontemayor@gmail.com>
2022-05-15 19:00:24 -0500

    Remove console log

--- backend/middleware/authenticateToken.js
+++ backend/middleware/authenticateToken.js
@@ -5,7 +5,6 @@ function authenticateToken(req, res, next) {
   const token = authHeader && authHeader.split(" ")[1];
 
   if (token == null) return res.sendStatus(401);
-  console.log("pasa");
 
   jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
     if (err) return res.sendStatus(403);
