4acca79e306075c2d21118701e6ed21589bd3695
nadia.co <ngarciaorozco@outlook.com>
2022-05-26 02:57:52 -0500

    create report

--- backend/routes/login.routes.js
+++ backend/routes/login.routes.js
@@ -1,51 +1,50 @@
-const express = require("express");
-const router = express.Router();
-const jwt = require("jsonwebtoken");
+const express = require('express')
+const router = express.Router()
+const jwt = require('jsonwebtoken')
 
-const User = require("../models/User");
+const User = require('../models/User')
 
+router.post('/', async (req, res) => {
+  const { email, password } = req.body
+  console.log(email)
 
-router.post("/", async (req, res) => {
-    const {email, password} = req.body;
-    console.log(email);
-
-    const user = await User.findOne({email: email.toLowerCase()});
-    if (!user) {
-        return res.status(404).json({
-            ok: false,
-            message: "User not found",
-        });
-    }else{
-        if(!user.activated){
-            return res.status(404).json({
-            ok: false,
-            message: "User not activated",
-        });
-
-        }
-        if (await user.validatePassword(password)) {
-            const token = jwt.sign({user}, process.env.SECRET_KEY, {
-                expiresIn: "24h",
-            });
-            const usr = {
-                id: user._id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                tower: user.tower,
-                role : user.role
-            }
-            res.status(200).json({
-                ok: true,
-                user: usr,
-                token: token
-            });
-        }else{
-            res.status(400).json({
-                ok: false,
-                message: "Invalid password"
-            });
-        }
+  const user = await User.findOne({ email: email.toLowerCase() })
+  if (!user) {
+    return res.status(404).json({
+      ok: false,
+      message: 'User not found',
+    })
+  } else {
+    if (!user.activated) {
+      return res.status(404).json({
+        ok: false,
+        message: 'User not activated',
+      })
     }
-});
+    if (await user.validatePassword(password)) {
+      const token = jwt.sign({ user }, process.env.SECRET_KEY, {
+        expiresIn: '24h',
+      })
+      const usr = {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        tower: user.tower,
+        apartment: user.apartment,
+        role: user.role,
+      }
+      res.status(200).json({
+        ok: true,
+        user: usr,
+        token: token,
+      })
+    } else {
+      res.status(400).json({
+        ok: false,
+        message: 'Invalid password',
+      })
+    }
+  }
+})
 
-module.exports = router;
+module.exports = router
